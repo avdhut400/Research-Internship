bb37e7998cc046cb93a4df5bb949293209e1c62e
avdhut400 <avdhutmagar@gmail.com>
2026-08-01 04:33:31 +0530

    bug fix

--- frontend/src/services/predictionService.js
+++ frontend/src/services/predictionService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
+// const API_URL = "http://localhost:5000/api/predict";
 const API_URL = `${
   import.meta.env.VITE_API_URL || "http://localhost:5000"
-}/api/feedback`;
-
+}/api/predict`;
 export const predictOil = async (imageFile) => {
   const formData = new FormData();
 
